f17aa8ac39f3d97432327fcec32aedb58c19dd5f
Dan Hart <dan-hart@users.noreply.github.com>
2022-05-16 13:54:23 -0400

    Add Locomotive

---
+++ Sources/WMATASwift/Locomotive/Locomotive.swift
@@ -0,0 +1,32 @@
+//
+//  Locomotive.swift
+//  
+//
+//  Created by Dan Hart on 5/16/22.
+//
+
+import Foundation
+#if canImport(SwiftUI)
+import SwiftUI
+
+class Locomotive: ObservableObject {
+    @Published var stations: [Station] = []
+    
+    // MARK: - Timer
+    var fetchTimer: Timer?
+    
+    init() {
+        fetchTimer = Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(Locomotive.fetch), userInfo: nil, repeats: true)
+    }
+
+    @objc func fetch() {
+        Task {
+            let fetchedStations = try await Conductor.shared.railStations()
+            if !fetchedStations.isEmpty {
+                stations = fetchedStations
+            }
+        }
+    }
+}
+
+#endif
